71ffcc87936113674761e5801acfd2cda62b54ec
Viktor <3952324@gmail.com>
2023-10-03 12:46:50 +0200

    init commit

---
+++ src/main/java/com/github/viktor2308/studenttesttask/StudentTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.github.viktor2308.studenttesttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentTestTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentTestTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/viktor2308/studenttesttask/StudentTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.viktor2308.studenttesttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentTestTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
